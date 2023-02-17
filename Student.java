9f346c1e2679fba4961d1911d5fece01f3c138b1
Adalberto <21670125@itiguala.edu.mx>
2023-02-17 01:04:53 -0600

    Se subió la clase de average

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name; //Jorge
+    private String lastname; //Perez
+    private Short semester; //6
+    private long controlnumber; // 12345678
+    private String career; // English teacher
+    private double average; // 9.5
+
+
+}
+
+
